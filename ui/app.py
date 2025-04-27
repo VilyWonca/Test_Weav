3f1bcb18398bd8e0104775b0431baf7c1014fc6b
VilyWonca <delovoder@gmail.com>
2025-04-27 21:05:50 +0300

    add non-fix app.py

--- ui/app.py
+++ ui/app.py
@@ -7,23 +7,6 @@ import streamlit as st
 
 st.title("S_MAKE")
 
-if "messages" not in st.session_state:
-    st.session_state.messages = []
-
-for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
-
-if prompt := st.chat_input("Как дела?"):
-    st.session_state.messages.append({"role": "user", "content": prompt})
-    with st.chat_message("user"):
-        st.markdown(prompt)
-
-    with st.chat_message("assistant"):
-        stream = chat(
-            model='owl/t-lite:latest',
-            messages=[{'role': 'user', 'content': prompt}],
-            stream=True,
-        )
-        response = st.write_stream(i['message']['content'] for i in stream)
-    st.session_state.messages.append({"role": "assistant", "content": response})
+prompt = st.chat_input("Say something")
+if prompt:
+    st.write(f"User has sent the following prompt: {prompt}")
