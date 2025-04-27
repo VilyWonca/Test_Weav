d70b17a8fe6d3862a5dd96796c8091d0becafec5
VilyWonca <delovoder@gmail.com>
2025-04-27 11:17:30 +0300

    add fix parserpdf

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -5,13 +5,22 @@ class PDFParser:
     def __init__(self, file_name: str):
         self.file_name = file_name
     
-    def clean_text(text: str) -> str:
-        
+    def clean_text(self, text: str) -> str:
+        text = text.replace('\n', '').replace('\r', '').replace('\t', '').replace('\xa0', '')
+        return text
 
-    def extract_text(self, file_name: str) -> list[str]:
-        doc = pymupdf.open(file_name)
+    def extract_text(self) -> list[str]:
+        try:
+            doc = pymupdf.open(self.file_name)
+            
+        except Exception as e:
+            print(f"Возникла ошибка при открытии файла: {e}")
+            return []
         out = []
         for page in doc:
-            text = page.get_text().encode("utf-8")
-            out.append(text)
+            text = page.get_text()
+            clear_text = self.clean_text(text)
+            out.append(clear_text)
+        doc.close()
         return out
+    
